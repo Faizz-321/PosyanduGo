a823bd33d01b5fcd1c20986a6ed2e7fad900112e
Muhammad Faiz <muhfaiz030705@gmail.com>
2026-09-18 14:30:30 +0800

    chore: Update default Puskesmas credentials

--- database.sql
+++ database.sql
@@ -220,5 +220,5 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Data Default (Kader dan Puskesmas)
 INSERT IGNORE INTO users (username, password, role, nama_posyandu) VALUES 
-('puskesmas', 'puskesmas123', 'Puskesmas', NULL);
+('PuskesmassCempae@gmail.com', 'PuskesmasCempae', 'Puskesmas', NULL);
 
